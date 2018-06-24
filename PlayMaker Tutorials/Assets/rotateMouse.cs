220ccb9f468bf5487cc8b8582b068b88b09bb2ac
phits <john@fitz.com>
2018-06-24 13:14:30 -0400

    Player running

---
+++ PlayMaker Tutorials/Assets/rotateMouse.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotateMouse : MonoBehaviour {
+
+	public float horizontalSpeed = 2.0F;
+	public float m_Speed = 5.0F;
+	Rigidbody m_Rigidbody;
+    // public float verticalSpeed = 2.0F;
+
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	    float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        // float v = verticalSpeed * Input.GetAxis("Mouse Y");
+        transform.Rotate(0, h, 0);
+		m_Rigidbody.velocity = transform.forward * m_Speed;
+		
+	}
+}
